ca8d7a7e4287ed8d8652b08234ee6d6988c7bc76
jakemaguy <36657294+jakemaguy@users.noreply.github.com>
2018-02-26 15:03:26 -0500

    Update Stellrium_test2.ino

--- Stellrium_test2.ino
+++ Stellrium_test2.ino
@@ -9,8 +9,9 @@
 Adafruit_BNO055 bno = Adafruit_BNO055();
 RTC_DS3231 rtc;
 
-float jd(int y, int m, int d);
-float gst(float jd, float ut);
+// polaris Equitorial Coordinates
+float poleAR = 2.9203;
+float poleHA = 21.3362;
 
 /* Global Variable Definitions */
 char txAR[10];
@@ -19,19 +20,20 @@ char input[20];
 char SIGNtel;
 double arHH, arMM, arSS;
 double decDEG, decMM, decSS; 
-float dec, ra;
+double dec, ra;
 
 /* Lattiude/Longitude - edit the number to correspond with your location */
-float lat = 42.65176250;
-float lon = -71.315/15;        //longitude is west so this value is negative
+float lat = 42.507416*PI/180;
+float lon = -71.81309;        //longitude is west so this value is negative
 int TIMEZONE = -5;  // eastern time GMT: -5
 int DST = 0;                // Daylight Savings Time (1 = summer, 0 = winter)
+double LST, ALT, AZ;
 
 /* prototype functions */
 void convertEQ();
 void transmitAR();
 void transmitDEC();
-double returnLST(int y, int m, int d, int h, int mn, int s);
+//void LST_get();
 
 void setup() {
   // put your setup code here, to run once:
@@ -44,10 +46,13 @@ void setup() {
   rtc.begin();
   //Wire.begin();
   bno.setExtCrystalUse(true);
+  LST = poleAR + poleHA;
+  while (LST >= 24) LST -= 24;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  convertEQ();
   if (Serial.available()>0){         // ottiene byte in entrata
     communication();
   }
@@ -59,7 +64,6 @@ void loop() {
 void communication(){
   
   int i=0;
-  convertEQ();
   input[i++] = Serial.read();
   delay(5);
   while((input[i++] = Serial.read()) != '#'){
@@ -67,6 +71,7 @@ void communication(){
   }
   input[i]='\0';
   
+    
   if(input[1]==':' && input[2]=='G' && input[3]=='R' && input[4]=='#'){             // con il comando #:GR# stellarium chiede l'invio della coordinata di AR
     transmitHA();
   }
@@ -80,45 +85,42 @@ void communication(){
 //-----------------------------------------------------------------------------------------------------------------------
 
 void convertEQ(){
-  double alt, az, dec_sin, ha_cos, ha, ha_prime, sin_A;
+  /* Local Variable Definitons */
+  float dec_sin, ha_cos, ha, ha_prime, sin_A;
   /* Get a new sensor event */ 
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
   DateTime now = rtc.now();
-  
-  az = radians(euler.x());
-  alt = radians(euler.y());
-  
-  dec_sin=((sin(alt)*sin(lat))+(cos(alt)*cos(lat)*cos(az)));
-  dec=asin(dec_sin);
 
-  ha_cos=((sin(alt)-(sin(lat)*sin(dec)))/(cos(lat)*cos(dec)));
-  ha_prime=acos(ha_cos);
-  
-  dec=degrees(dec);
-  ha_prime=degrees(ha_prime);
+  //converts AZ and ALT to radians becuase Arduino math take in radians as args
+  AZ = euler.x()*PI/180.0;
+  ALT = euler.y()*PI/180.0;
+  //ALT = -47.0;
 
-  sin_A = sin(az);
-  sin_A = degrees(sin_A);
-  if (sin_A > 0){
-    ha = 360 - ha_prime; 
+  dec_sin = ((sin(ALT) * sin(lat)) + (cos(ALT) * cos(lat) * cos(AZ)));
+  dec = asin(dec_sin);
+
+  ha_cos = ((sin(ALT) - (sin(lat) * sin(dec))) / (cos(lat) * cos(dec)));
+  ha_prime = acos(ha_cos);
+
+  dec = dec*180.0/PI;
+  //dec = abs(dec); 
+  ha_prime = ha_prime*180.0/PI;
+
+  sin_A = sin(AZ);
+  sin_A = sin_A*180.0/PI;
+  if (sin_A >= 0) {
+    ha = 360.0 - ha_prime;
   }
-  if (sin_A < 0){
+  else {
     ha = ha_prime;
   }
-  // calculate lst here
-  ha = ha / 15;
-  float seconds = now.second();
-  float minutes = now.minute();
-  float hours = now.hour();
-  float local_time = (seconds / 60);
-  local_time = ((local_time + minutes) / 60);
-  local_time = (local_time + hours);          // hours are returned in 24 hour format via DS3231
-  local_time = (local_time - DST);            // zone time
-  float UT = (local_time - TIMEZONE);
-  float JD = jd(now.year(), now.month(), now.day());
-  float GST = gst(JD, UT);
-  float LST = GST + lon;
+  ha = ha / 15.0;
+  
+  //LST_get();
   ra = LST - ha;
+  while (ra >= 24) ra -= 24;
+  while (ra < 0) ra += 24.0;
+  
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------   
@@ -137,92 +139,63 @@ void transmitHA(){                                                         // tr
 
 void transmitDEC(){                                                             // trasmissione dati nella forma  sDDßMM:SS#  (dove s è il segno + o -)
      //convertEQ();
-     dec = modf(dec, &decDEG);
-     (decDEG < 0) ? SIGNtel = 45: SIGNtel = 43;                                 // controllo del segno della coordinata DEC del telescopio
-     dec = dec * 60;
-     dec = modf(dec, &decMM);
-     decSS = dec * 60;
-     sprintf(txDEC, "%c%02d%c%02d:%02d#", SIGNtel, int(decDEG), 223, int(decMM), int(decSS));
-     Serial.print(txDEC);
-  
-    }
+#if 0
+  char sDEC_tel;
+  sprintf(buffer, "%lf", dec);
+  sscanf(buffer, "%lf.%lf", decDEG, dec);// controllo del segno della coordinata DEC del telescopio
+  (decDEG < 0) ? SIGNtel = 45: SIGNtel = 43;
+  if (dec < 0) dec *= -1;
+  dec = dec * 60;
+  memset(buffer, 0, sizeof(buffer));
+  sprintf(buffer, "%lf", dec);
+  sscanf(buffer, "%lf.%lf", decMM, dec);
+  decSS = dec * 60;
+  memset(buffer, 0, sizeof(buffer));
+#endif
+  char sDEC_tel;
+  decDEG = (int)dec;
+  decMM = (int)((dec-decDEG)*60.0);
+  decSS = (dec-decDEG-decMM/60.0)*3600.0;
+  (decDEG < 0) ? sDEC_tel = 45 : sDEC_tel = 43;
+  sprintf(txDEC, "%c%02d%c%02d:%02d#", sDEC_tel, (int)abs(decDEG), 223, (int)decMM, (int)decSS);
+  Serial.print(txDEC);
+}
     
 //-------------------------------------------------------------------------------------------------------------------------------------
-
-float jd(int y, int m, int d)
+#if 0
+void LST_get()
 {
-  if (m < 3) { m += 12; y -= 1; }
-  double A, B, C, E, F;
-  A = (long)y / 100;
-  B = (long)A / 4;
-  C = (long)2-A+B;
-  E = (long)(365.25*(y+4716));
-  F = (long)(30.6001*(m+1));
-  return (C + d + E + F - 1524.5);
-}
+  DateTime now = rtc.now();
+  int S = now.second();
+  int MN = now.minute();
+  int H = now.hour();
+  int D = now.day();
+  int M = now.month();
+  int Y = now.year();
 
-float gst(float jd, float ut)
-{
-  float S, T, T0, A;
+  if (M < 3) { M += 12; Y -= 1; }
+  double HH = H + ((float)MN/60.00) + ((float)S/3600.00);
+  float AA = (int)(365.25*(Y+4716)); 
+  float BB = (int)(30.6001*(M+1));
+  double CurrentJDN = AA + BB + D - 15375.5 + (HH - TIMEZONE)/24;
+  float CurrentDay = CurrentJDN - 2451543.5;
+
+  double MJD = CurrentJDN - 2400000.5;
+  int MDJ0 = (int)MJD;
+  float ut = (MJD - MDJ0)*24.0;
+  double t = (MDJ0-51544.5)/36525.0;
+  double GMST = 6.697374558 + 1.0027379093*ut + (8640184.812866 + (0.093104 - 0.0000062*t)*t)*t/3600.0;
+
+  double GMST = 18.697374558 + 24.06570982441908*D;
+  int GMSTint = (int)GMST;
+  GMSTint/=24;
+  GMST = GMST - (double)GMSTint* 24;
   
-  S = jd - 2451545.0;
-  T = (S / 36525.0);
-  T0 = (6.697374558 + (2400.051336 * T) + (0.000025862 * (T * T)));
-  T0 = T0 - (24 * (T0 / 24));
-  ut = ut * 1.002637909;
-  A = ut + T0;
-  if (A < 0)
-    A = A - (A * (A / 24));
-  return A;
+  LST = GMST + lon/15;
+  
+  int LSTint = (int)LST;
+  LSTint/=24;
+  LST = LST - (double)LSTint *24; 
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-double returnLST(int y, int m, int d, int h, int mn, int s)
-{
-}
-*/
-
-/*
-void convertDEC(){
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  degDEC = (euler.y());
-  degDEC = modf(degDEC, &degree);
-  if (degree < 0){
-    Serial.print("-");
-    if (degree > -10){
-      Serial.print("0");
-    }
-  }
-  else if (degree >= 0){
-    Serial.print("+");
-    if (degree < 10){
-      Serial.print("0");
-    }
-  }
-  Serial.print(degree, 0);
-  Serial.print ((char)223);
-  degDEC = degDEC * 60;
-  degDEC = modf(degDEC, &deg_DECmin);
-  if (deg_DECmin < 10){
-    Serial.print("0");
-  }
-  Serial.print(deg_DECmin, 0);
-  Serial.print(":");
-  deg_DECsec = degDEC * 60;
-  if (deg_DECsec < 10){
-    Serial.print("0");
-  }
-  Serial.print(deg_DECsec, 0);
-}
-*/
+#endif
 
